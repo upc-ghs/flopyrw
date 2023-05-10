e34faaa732e19f0dc6fff09a0325b9c8c54ecd95
Rodrigo Pérez <rodrigo.alfonso.perez@upc.edu>
2023-05-10 10:36:53 +0200

    [utils] count_instances decorator to utils

---
+++ flopyrw/utils.py
@@ -0,0 +1,14 @@
+'''
+General utilities 
+'''
+
+
+def count_instances(method):
+    '''
+    Decorator for counting class instances. 
+    Rely on the class having a COUNTER variable
+    '''
+    def wrapper(self, *args, **kw):
+        self.__class__.COUNTER += 1
+        return method(self, *args, **kw)
+    return wrapper
