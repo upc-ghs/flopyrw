ae2328eb2ede81f8f9889c6fa14aae8bdbcdaa23
Rodrigo Perez <rodrigo.alfonso.perez@upc.edu>
2023-05-11 14:44:46 +0200

    [spc] doc for the package and update writing to be consistent with program structure

--- flopyrw/mprwspc.py
+++ flopyrw/mprwspc.py
@@ -20,12 +20,21 @@ class ModpathRWSpc( Package ):
     Parameters
     ----------
     model : model object
-        The model object (of type :class:`flopy.modpath.Modpath7`) to which
+        The model object (of type :class: ModpathRW) to which
         this package will be added.
     dispersion: ModpathRWDsp object
-        Defines dispersion model and properties to this specie.
-    pgroups: list, np.array
-        Particle groups related to this specie.
+        Defines dispersion model and properties for this solute. Is a required foreign key. 
+    pgroups : list, np.array (int)
+        Particle groups related to this specie. These are interpreted only if 
+        particlesmassoption != 2. In case the simulation is configured with 
+        particlesmassoption == 2, then the particle groups associated to a 
+        specie are interpreted from the specified solute ids for each particle group.
+        These ids are zero based (while writing is corrected to one based) and should 
+        be consistent with the order in which the particle groups are specified.
+    stringid : str
+        String identifier for this species. 
+    extension : str
+        The extension for the species file, by default spc. 
     """
 
 
@@ -34,14 +43,13 @@ class ModpathRWSpc( Package ):
     UNITNUMBER = 0
     INSTANCES  = []
 
-
     @count_instances
     def __init__(
         self,
         model,
         dispersion, # it's like a foreign key
         pgroups    = None,
-        id         = None,
+        id         = None, # internal
         stringid   = None,
         extension  = 'spc',
     ):
@@ -52,40 +60,42 @@ class ModpathRWSpc( Package ):
             super().__init__(model, extension, "SPC", unitnumber)
             self.__class__.UNITNUMBER = self.unit_number[0]
         else:
-            # Needed for Util3d
             self._parent = self.INSTANCES[0]._parent
 
         # Assign dispersion
         if not isinstance( dispersion, ModpathRWDsp ):
-            raise Exception('flopyrw:ModpathRWSpc: dispersion parameter should be of ModpathRWDsp type. Given ', type( dispersion ) )
+            raise ValueError(
+                self.__class__.__name__ + ':' + 
+                ' Invalid dispersion parameter of type ' +str(type(dispersion))+
+                '. It should be type ModpathRWDsp. '
+            )
         self.dispersion = dispersion
 
-        # Assign pgroups id's
-        if pgroups is None: 
-            warnings.warn("flopyrw:ModpathRWSpc: no pgroups were specified, defaults to pgroups = [0]")
-            pgroups = [0]
-            # Or leave as None
-        if not isinstance(pgroups, (list,np.array)):
-            raise Exception('flopyrw:ModpathRWSpc: pgroups should be of type list or np.array. Given ', type( pgroups ) )
-        if isinstance( pgroups, list ):
-            pgroups = np.array( pgroups )
-        # Requires some more sanity checks to verify integer id's and so on
-        # Might be worth considering removing particle groups altogether and extend 
-        # species from those classes.
+        # Will be written only if particlesmassoption != 2
+        if pgroups is not None:
+            if not isinstance(pgroups, (list,np.array)):
+                raise ValueError(
+                    self.__class__.__name__ + ':' + 
+                    ' Invalid pgroups parameter of type ' +str(type(pgroups))+
+                    '. It should be of type list or np.array.'
+                )
+            if isinstance( pgroups, list ):
+                pgroups = np.array( pgroups )
+        # Requires some more sanity checks to verify integer id's.
+        # Might be worth considering a verification versus already 
+        # specified particle groups, which in the end would require
+        # forcing giving this package after IC, SRC.
         self.pgroups = pgroups
         
-
         # Define id
         if (id is not None): 
             self.id = id
         else:
             self.id = self.__class__.COUNTER
-
         if (stringid is not None): 
             self.stringid = stringid
         else:
             self.stringid = 'SPC'+str(self.__class__.COUNTER)
-
 
         # Add package and save instance        
         if self.__class__.COUNTER == 1:
@@ -118,9 +128,6 @@ class ModpathRWSpc( Package ):
 
         for ins in self.INSTANCES:
 
-            # Write id
-            f.write(f"{ins.id}\n")
-
             # Write stringid
             f.write(f"{ins.stringid}\n")
 
@@ -129,16 +136,28 @@ class ModpathRWSpc( Package ):
             # If particlesmassoption equal 2, then 
             # soluteId is infered from given particlegroups
             if ( self.INSTANCES[0]._parent.particlesmassoption != 2 ):
-                # Write the number of groups
-                f.write(f"{len(ins.pgroups)}\n")
+                if ( 
+                    ( ins.pgroups is not None ) and
+                    ( len(ins.pgroups) > 0 )
+                ):
+                    # Write the number of groups
+                    f.write(f"{len(ins.pgroups)}\n")
 
-                for idpg, pg in enumerate(ins.pgroups):
-                    # Write the pgroup id in the list of pgroups
-                    f.write(f"{pg+1}\n")
+                    for idpg, pg in enumerate(ins.pgroups):
+                        # Write the pgroup id in the list of pgroups
+                        # Notice the plus one.
+                        f.write(f"{pg+1}\n")
+                else:
+                    raise ValueError(
+                        self.__class__.__name__ + ':' + 
+                        ' Invalid pgroups parameter of type ' +str(type(ins.pgroups))+
+                        '. It should be of type list or np.array with at least one element.'
+                    )
 
-            # Write dispersion "foreign key"        
-            f.write(f"{ins.dispersion.id}\n")
-            f.write(f"{ins.dispersion.stringid}\n")
+            # Write dispersion "foreign key", only if
+            # speciesdispersionoption == 1
+            if  self.INSTANCES[0]._parent.speciesdispersionoption == 1:
+                f.write(f"{ins.dispersion.stringid}\n")
 
 
         # Done
