ae8eb19a883f1032b5278d13f8cb8067dbe9d71d
Rodrigo Perez <rodrigo.alfonso.perez@upc.edu>
2023-05-22 13:30:19 +0200

    [ic,test,multipackae] update ic implementation to multipackage decorator and initial test case verifying input parameters

---
+++ autotest/test_mprw_ic.py
@@ -0,0 +1,115 @@
+'''
+Tests for the ModpathRWSpc class
+'''
+
+import numpy as np
+import pytest
+
+from autotest.test_mprw_p09mt3d_cases import MT3DP09Cases
+from flopy import modpathrw
+
+
+def test_mprw_ic_input_mf6(function_tmpdir):
+    '''
+    Verifies the input for species class
+    '''
+
+    # get the mf6 case
+    # brings WEL-1 and CHD-1 with aux CONCENTRATION
+    flowmf6 = MT3DP09Cases.mf6(function_tmpdir) 
+
+    # modpath-rw
+    mp = modpathrw.ModpathRW(
+            modelname='mprwsim',
+            flowmodel=flowmf6,
+            model_ws =function_tmpdir,
+        )
+   
+    # dsp 
+    modpathrw.ModpathRWDsp(mp)
+
+    # rwopts
+    modpathrw.ModpathRWOpts(mp)
+
+
+    # simple checks #
+    #---------------#
+    with pytest.raises(ValueError):
+        # define with None concentration
+        modpathrw.ModpathRWIc(mp, concentration=None)
+    with pytest.raises(TypeError):
+        # define with invalid concentration list 
+        modpathrw.ModpathRWIc(mp, concentration=['asd'])
+    with pytest.raises(Exception):
+        # define with invalid concentration shape 
+        modpathrw.ModpathRWIc(mp, concentration=[2,1])
+
+    # create a valid concentration
+    mgrid = flowmf6.modelgrid
+    concentration = np.ones(shape=(mgrid.nlay,mgrid.nrow,mgrid.ncol))
+
+    with pytest.raises(ValueError):
+        # define with zero mass 
+        modpathrw.ModpathRWIc(mp, concentration=concentration, particlesmass=0.0)
+    with pytest.raises(ValueError):
+        # define with negative mass 
+        modpathrw.ModpathRWIc(mp, concentration=concentration, particlesmass=-10.0)
+    with pytest.raises(ValueError):
+        # define with invalid species id 
+        modpathrw.ModpathRWIc(mp, concentration=concentration, speciesid=-9)
+    with pytest.raises(TypeError):
+        # define with invalid species id 
+        modpathrw.ModpathRWIc(mp, concentration=concentration, speciesid='aspecies' )
+
+    # sim with particlesmassoption = 2
+    simconfig = {
+        'simulationtype'         : 'rwtimeseries', 
+        'trackingdirection'      : 'forward',
+        'weaksinkoption'         : 'stop_at',
+        'weaksourceoption'       : 'pass_through',
+        'referencetime'          : 0.0,
+        'stoptimeoption'         : 'specified',
+        'stoptime'               : 2*365*86400,
+        'particlesmassoption'    : 2,
+        'speciesdispersionoption': 0,
+        'timeseriesoutputoption' : 2, 
+        'timepointdata'          : [2, (1.0*365*86400)],
+    }
+    mpsim = modpathrw.ModpathRWSim(
+        mp,
+        **simconfig
+    )
+    # ic: by default speciesid = 0
+    modpathrw.ModpathRWIc(mp, concentration=concentration )
+
+    # write
+    mp.write_input()
+
+
+    # model with particlesmassoption = 0 #
+    #------------------------------------#
+    # modpath-rw
+    mp2 = modpathrw.ModpathRW(
+            modelname='mprwsim_2',
+            flowmodel=flowmf6,
+            model_ws =function_tmpdir,
+        )
+   
+    # dsp 
+    modpathrw.ModpathRWDsp(mp2)
+
+    # rwopts
+    modpathrw.ModpathRWOpts(mp2)
+
+    simconfig['particlesmassoption'] = 0
+    mpsim = modpathrw.ModpathRWSim(
+        mp2,
+        **simconfig
+    )
+
+    # ic
+    modpathrw.ModpathRWIc(mp2, concentration=concentration )
+
+    # write
+    mp2.write_input()
+
--- flopy/modpathrw/mprwic.py
+++ flopy/modpathrw/mprwic.py
@@ -10,7 +10,7 @@ from flopy.pakbase import Package
 from flopy.utils import Util3d
 
 # local 
-from .utils import count_instances # Increment COUNTER
+from .utils import multipackage
 
 
 class ModpathRWIc( Package ):
@@ -25,93 +25,102 @@ class ModpathRWIc( Package ):
     kind  : int 
         The kind of initial condition specification. In the meantime, there is only 
         one format enabled loading the concentration parameter with u3d.
-    mass  : float
+    particlesmass  : float
         The scale of particles mass used for transforming the concentration data into 
         a distribution of particles. 
     concentration : list, ndarray, int or float
-        Interpreted as a resident initial concentration. It will be written with Util3d.
+        Interpreted as a resident initial concentration. It will be written with Util3d, 
+        meaning that if the input is multidimensional, it should be consistent with 
+        flow model dimensions. 
     speciesid : int 
         The species id to which this initial concentration is related. Will only 
         be written if particlesmassoption == 2. It should be given as the zero-based 
         index of the specie (the python-index of the specie in the list of species), 
         but is written to the package file with fortran format (+1, see the write_file method).
-    id : int 
-        A positive integer identifier. If None given is automatically assigned with 
-        the instance counter. 
     stringid : str
-        A string identifier. If None is given is automatically generated as IC+str(id).
+        A string identifier. If None is given is automatically filled by 
+        concatenating the package ftype and an integer id. 
     extension : str
         The file extension for the package. Defaults to ic. 
     """
 
 
-    # Class properties
-    COUNTER    = 0
-    UNITNUMBER = 0
-    INSTANCES  = []
+    @staticmethod
+    def _ftype():
+        return 'IC'
 
 
-    @count_instances
+    @multipackage
     def __init__(
         self,
         model,
         kind          = 0   ,
-        mass          = 1.0 ,
-        concentration = None,
-        speciesid     = None,
-        id            = None,
+        particlesmass = 1.0 ,
+        concentration = 0.0 ,
+        speciesid     = 0   , 
         stringid      = None,
         extension     = 'ic',
     ):
 
-        if self.__class__.COUNTER == 1:
-            # Define UNITNUMBER if the first instance is created
-            unitnumber = model.next_unit()
-            super().__init__(model, extension, "IC", unitnumber)
-            self.__class__.UNITNUMBER = self.unit_number[0]
-        elif ( len(self.INSTANCES)==0 ):
-            # If counter was not one and no instances, treat it like the first
-            self.__class__.COUNTER = 1
-            unitnumber = model.next_unit()
-            super().__init__(model, extension, "IC", unitnumber)
-            self.__class__.UNITNUMBER = self.unit_number[0]
+        # Call parent constructor
+        ftype = self._ftype()
+        if ( model.multipackage[ftype]['count'] == 0 ): 
+            super().__init__(
+                model,
+                extension,
+                ftype,
+                model.multipackage[ftype]['unitnumber']
+            )
         else:
-            # pass the parent 
-            self._parent = self.INSTANCES[0]._parent
-
+            # Pass the parent
+            self._parent = model.multipackage[ftype]['instances'][0]._parent
 
         # Determines format for writing the initial condition
         if (kind not in [0]): 
             raise ValueError(
-                self.__class__.__name__ + ':' + 
-                ' Invalid kind parameter ' +str(kind)+
-                '. Allowed values are 0 (read concentration with u3d).'
+                self.__class__.__name__ + ':'
+                + ' Invalid kind parameter ' + str(kind)
+                + '. Allowed values are 0 (read concentration with u3d).'
             )
         self.kind = kind
 
         # The particles mass
-        if (mass <= 0):
-            raise ValueError(
-                self.__class__.__name__ + ':' + 
-                ' Invalid particle mass for initial condition. ' +
-                'It should be a positive value.' 
+        if ( not isinstance( particlesmass, (int,float) ) ):
+            raise TypeError(
+                self.__class__.__name__ + ':' 
+                + ' Invalid type for particlesmass. It should be int/float, '
+                + ' but ' + str(type(particlesmass)) + ' was given.' 
             )
-        self.mass = mass
+        if (particlesmass <= 0):
+            raise ValueError(
+                self.__class__.__name__ + ':' 
+                + ' Invalid particle mass for initial condition. '
+                + 'It should be a positive value.' 
+            )
+        self.particlesmass = particlesmass
 
         # A species id, will only be written 
         # if particlesmassoption == 2
+        if ( speciesid is not None ):
+            if not isinstance( speciesid, int ): 
+                raise TypeError(
+                    self.__class__.__name__ + ':' 
+                    + ' Invalid type for speciesid. It should be integer, '
+                    + ' but ' + str(type(speciesid)) + ' was given.' 
+                )
+            if (speciesid < 0):
+                raise ValueError(
+                    self.__class__.__name__ + ':' 
+                    + ' Invalid value for speciesid. It should be ' 
+                    + ' a valid zero-based index of species.' 
+                )
         self.speciesid = speciesid
 
-        # define id
-        if (id is not None): 
-            self.id = id
-        else:
-            self.id = self.__class__.COUNTER
-
+        # String id 
         if (stringid is not None): 
             self.stringid = stringid
         else:
-            self.stringid = 'IC'+str(self.id)
+            self.stringid = ftype+str(self.id)
 
         # and the concentration: needs id
         shape = model.shape
@@ -131,9 +140,9 @@ class ModpathRWIc( Package ):
             # TypeError if any other unsupported type
             if ( not isinstance( concentration, (list,float,int,np.ndarray) ) ):
                 raise TypeError(
-                    self.__class__.__name__ + ':' + 
-                    ' Invalid type ' + str(type(concentration)) + 
-                    ' for concentration. It should be list, np.ndarray or a single numeric value.'
+                    self.__class__.__name__ + ':'
+                    + ' Invalid type ' + str(type(concentration))
+                    + ' for concentration. It should be list, np.ndarray or a single numeric value.'
                 )
             # If list, to np.array
             if ( isinstance( concentration, list ) ): 
@@ -144,31 +153,32 @@ class ModpathRWIc( Package ):
                     (concentration.dtype not in [np.int32, np.int64, np.float32, np.float64])
                 ):
                     raise TypeError(
-                        self.__class__.__name__ + ':' + 
-                        ' Invalid dtype ' + str(concentration.dtype) + 
-                        ' for concentration. It should be int or float. ' 
+                        self.__class__.__name__ + ':' 
+                        + ' Invalid dtype ' + str(concentration.dtype) 
+                        + ' for concentration. It should be int or float. ' 
                     )
-            self.concentration = Util3d(
-                model,
-                shape3d,
-                np.float32,
-                concentration,
-                name=self.stringid,
-                locat=self.__class__.UNITNUMBER,
-            )
+            try:
+                self.concentration = Util3d(
+                    model,
+                    shape3d,
+                    np.float32,
+                    concentration,
+                    name=self.stringid,
+                    locat=self._parent.multipackage[ftype]['unitnumber'], 
+                )
+            except:
+                raise Exception(
+                    self.__class__.__name__ + ':'
+                    + ' Error while initializing distributed variable concentration. '
+                    + 'Is the input shape consistent with flow model dimensions ? '
+                )
 
         elif ( (self.kind == 0) and (concentration is None) ):
             raise ValueError(
-                self.__class__.__name__ + ':' + 
-                ' Invalid initial concentration distribution. None was given.'
+                self.__class__.__name__ + ':'
+                + ' Invalid initial concentration. None was given.'
             )
-     
-
-        # Add package and save instance        
-        if self.__class__.COUNTER == 1:
-            self.parent.add_package(self)
-        self.__class__.INSTANCES.append( self )
-
+    
 
         # Done
         return
@@ -186,47 +196,47 @@ class ModpathRWIc( Package ):
         None
         """
 
+        # Shortcut to multipackage dict
+        mpkg = self.parent.multipackage[self._ftype()]
+        
+        # Write
+        with open( mpkg['instances'][0].fn_path, 'w' ) as f:
 
-        # Open file for writing
-        f = open(self.INSTANCES[0].fn_path, "w")
+            # Write how many instances...
+            f.write(f"{mpkg['count']}\n")
 
-        # Write how many INSTANCES 
-        # and loop over them
-        f.write(f"{self.COUNTER}\n")
+            # ...and loop over them
+            for ins in mpkg['instances']:
 
-        for ins in self.INSTANCES:
+                # Write string id 
+                f.write(f"{ins.stringid}\n")
 
-            # Write string id 
-            f.write(f"{ins.stringid}\n")
+                # Kind/format of initial condition 
+                f.write(f"{ins.kind}\n")
 
-            # Kind/format of initial condition 
-            f.write(f"{ins.kind}\n")
+                # 0: resident concentration array  
+                if ins.kind == 0:
 
-            # 0: resident concentration array  
-            if ins.kind == 0:
+                    # Give particles mass 
+                    f.write(f"{ins.particlesmass:.10f}\n")
 
-                # Give particles mass 
-                f.write(f"{ins.mass:.10f}\n")
-
-                # If solute ids are being specified, write the id
-                if( 
-                    ( self.INSTANCES[0]._parent.particlesmassoption == 2 ) 
-                ):
-                    if ( 
-                        ( ins.speciesid is None ) or
-                        ( ins.speciesid < 0 ) 
+                    # If solute ids are being specified, write the id
+                    if( 
+                        ( self._parent.particlesmassoption == 2 ) 
                     ):
-                        raise ValueError(
-                            self.__class__.__name__ + ':' + 
-                            ' Invalid species id for initial condition. ' +
-                            'It should be a valid index in the list of species.' 
-                        )
-                    f.write(f"{ins.speciesid+1}\n")
+                        if ( 
+                            ( ins.speciesid is None ) or
+                            ( ins.speciesid < 0 ) 
+                        ):
+                            raise ValueError(
+                                self.__class__.__name__ + ':'
+                                + ' Invalid species id for initial condition. '
+                                + 'It should be a valid index in the list of species.' 
+                            )
+                        f.write(f"{ins.speciesid+1}\n")
 
-
-                # And the concentration distribution
-                f.write(ins.concentration.get_file_entry())
-
+                    # And the concentration distribution
+                    f.write(ins.concentration.get_file_entry())
 
         # Done
         return
