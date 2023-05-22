af113a5110525220479d261da5cd6f816e9e808c
Rodrigo Perez <rodrigo.alfonso.perez@upc.edu>
2023-05-22 17:26:21 +0200

    [mprwbas,test] add valdiation layer for porosity at the mprwbas package and test

---
+++ autotest/test_mprw_bas.py
@@ -0,0 +1,61 @@
+'''
+Tests for the ModpathRWBas class
+'''
+
+import pytest
+import numpy as np
+
+from autotest.test_mprw_p09mt3d_cases import MT3DP09Cases
+from flopy import modpathrw
+
+
+def test_mprw_bas_input_mf6(function_tmpdir):
+    '''
+    Verifies the input for the basic class 
+    '''
+
+    # get the mf6 case
+    # brings WEL-1 and CHD-1 with aux CONCENTRATION
+    flowmf6 = MT3DP09Cases.mf6(function_tmpdir) 
+
+    # modpath-rw
+    mp = modpathrw.ModpathRW(
+            modelname='mprwsim',
+            flowmodel=flowmf6,
+            model_ws =function_tmpdir,
+        )
+   
+    # write distributed # 
+    #-------------------#
+    mgrid = flowmf6.modelgrid
+    zeroporosity = np.zeros(shape=(mgrid.nlay,mgrid.nrow,mgrid.ncol), dtype=np.int32)
+    badporosity = np.zeros(shape=(mgrid.nlay,mgrid.nrow,mgrid.ncol), dtype=np.int32)
+    badporosity[0,3,6] = 2.0 
+    badporosity[0,3,8] = -2.0 
+    porosity = 0.3*np.ones(shape=(mgrid.nlay,mgrid.nrow,mgrid.ncol), dtype=np.int32)
+
+
+    with pytest.raises(ValueError):
+        # define with only zeros
+        modpathrw.ModpathRWBas(mp, porosity=zeroporosity )
+    with pytest.raises(ValueError):
+        # define with array containing values outside the range
+        modpathrw.ModpathRWBas(mp, porosity=badporosity )
+    with pytest.raises(ValueError):
+        # define with array containing values outside the range
+        modpathrw.ModpathRWBas(mp, porosity=-1.0 )
+    with pytest.raises(TypeError):
+        # define with wrong type
+        modpathrw.ModpathRWBas(mp, porosity='porosity' )
+
+    # define consistent package 
+    bas = modpathrw.ModpathRWBas(mp, porosity=porosity)
+
+    # verify assignment to the main model
+    pkgs = mp.get_package_list()
+    assert bas._ftype() in pkgs, (
+            f"MPRWBAS package was not found in ModpathRW object"
+        )
+
+    # and write (without checking model consistency, check=False by default)
+    mp.write_input()
--- flopy/modpathrw/mprwbas.py
+++ flopy/modpathrw/mprwbas.py
@@ -2,9 +2,11 @@
 Configuration of MODPATH-RW basic package
 '''
 
+# python 
+import numpy as np
+
 # flopy 
 from flopy.modpath import Modpath7Bas
-
 
 class ModpathRWBas( Modpath7Bas ):
     '''
@@ -13,11 +15,50 @@ class ModpathRWBas( Modpath7Bas ):
     Extends flopy.modpath.Modpath7Bas
     '''
 
+    @staticmethod
+    def _ftype():
+        return 'MPRWBAS'
+
     def __init__(self, *args, **kwargs):
+        
+        # Add a layer of validation for porosity values
+        if 'porosity' in kwargs.keys():
+            if ( not isinstance( kwargs['porosity'], (float,list,np.ndarray)) ):
+                raise TypeError(
+                    f"{self.__class__.__name__} :"
+                    f" Invalid type for porosity. It should be real values, but "
+                    f"{str(type(kwargs['porosity']))} was given."
+                )
+            if ( isinstance( kwargs['porosity'], float ) ): 
+                kwargs['porosity'] = [kwargs['porosity']]
+            if ( isinstance( kwargs['porosity'], list ) ): 
+                kwargs['porosity'] = np.array(kwargs['porosity'])
+            if ( kwargs['porosity'].dtype not in [np.float32, np.float64, np.int32, np.int64 ] ):
+                raise TypeError(
+                    f"{self.__class__.__name__} :"
+                    f" Invalid type for porosity. It should be real values, but "
+                    f"{str(kwargs['porosity'].dtype)} was given."
+                )
+            # Verify that all values are within a valid range
+            if ( np.any( kwargs['porosity'] <= 0.0 ) or np.any( kwargs['porosity'] >= 1.0 ) ): 
+                raise ValueError(
+                    f"{self.__class__.__name__} :"
+                    f" Invalid values for porosity. It should contain real values " 
+                    f"only in the range [0.0,1.0]."
+                )
+            # Complain if only zeros
+            if ( np.all( kwargs['porosity'] == 0.0 ) ): 
+                raise ValueError(
+                    f"{self.__class__.__name__} :"
+                    f" Invalid values for porosity. It should contain real values " 
+                    f"only in the range [0.0,1.0], but only zeros were found."
+                )
+
         # Call parent constructor
         super().__init__(*args,**kwargs)
 
         # Not the most elegant solution but consistent
-        self.name  = ['MPRWBAS']
-        self._name = ['MPRWBAS']
+        ftype = self._ftype()
+        self.name  = [ftype]
+        self._name = [ftype]
         self._generate_heading()
